4d1aa082bb88a38256526bb58ac29a793cb2ca49
essalhi-moussa <essalhi.me01@gmail.com>
2024-09-15 22:51:18 +0100

    complate the sign in page functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,5 +1,141 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Signin() {
-  return <div>Signin</div>;
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate('/')
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div class="mt-2">
+            <input
+              type="eamil"
+              placeholder="Email"
+              id="email"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-3"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Password
+          </label>
+          <div class="mt-2">
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-3"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          disabled={loading}
+          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="">
+        <a
+          href="#"
+          class="flex items-center justify-center space-x-2 px-4 py-2 bg-white text-gray-700 border border-gray-300 rounded-md shadow-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-2"
+        >
+          <svg
+            class="w-5 h-5"
+            viewBox="0 0 21 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_13183_10121)">
+              <path
+                d="M20.3081 10.2303C20.3081 9.55056 20.253 8.86711 20.1354 8.19836H10.7031V12.0492H16.1046C15.8804 13.2911 15.1602 14.3898 14.1057 15.0879V17.5866H17.3282C19.2205 15.8449 20.3081 13.2728 20.3081 10.2303Z"
+                fill="#3F83F8"
+              ></path>
+              <path
+                d="M10.7019 20.0006C13.3989 20.0006 15.6734 19.1151 17.3306 17.5865L14.1081 15.0879C13.2115 15.6979 12.0541 16.0433 10.7056 16.0433C8.09669 16.0433 5.88468 14.2832 5.091 11.9169H1.76562V14.4927C3.46322 17.8695 6.92087 20.0006 10.7019 20.0006Z"
+                fill="#34A853"
+              ></path>
+              <path
+                d="M5.08857 11.9169C4.66969 10.6749 4.66969 9.33008 5.08857 8.08811V5.51233H1.76688C0.348541 8.33798 0.348541 11.667 1.76688 14.4927L5.08857 11.9169Z"
+                fill="#FBBC04"
+              ></path>
+              <path
+                d="M10.7019 3.95805C12.1276 3.936 13.5055 4.47247 14.538 5.45722L17.393 2.60218C15.5852 0.904587 13.1858 -0.0287217 10.7019 0.000673888C6.92087 0.000673888 3.46322 2.13185 1.76562 5.51234L5.08732 8.08813C5.87733 5.71811 8.09302 3.95805 10.7019 3.95805Z"
+                fill="#EA4335"
+              ></path>
+            </g>
+            <defs>
+              <clipPath id="clip0_13183_10121">
+                <rect
+                  width="20"
+                  height="20"
+                  fill="white"
+                  transform="translate(0.5)"
+                ></rect>
+              </clipPath>
+            </defs>
+          </svg>
+          <span>Sign up with Google</span>
+        </a>
+        <p class="text-sm  text-gray-900 dark:text-gray-900 mt-2">
+          Don't have an account?
+          <Link
+            to="/sign-up"
+            className="font-medium text-indigo-600 hover:underline dark:text-primary-500"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+      <p className="text-red-700 mt-4">{error && "Something went wrong"}</p>
+    </div>
+  );
 }
